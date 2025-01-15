30665c85ea30deb9cd3be21262be9539428bea2f
alex <alex@stylemi.co>
2025-01-15 17:37:45 +0200

    updated package name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='feed-sdk',
+    name='ebay-feed-sdk',
     version='1.0.0',
     packages=find_packages(),
     include_package_data=True,
